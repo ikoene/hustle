483811ecb2fe5f5102fa2a3801c78495f1a4278a
Koen Vinken <koen.vinken@wijs.be>
2018-08-13 21:48:54 +0200

    Initial Commit

---
+++ hustle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hustle
+//
+//  Created by Koen Vinken on 13/08/2018.
+//  Copyright © 2018 Koen Vinken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
